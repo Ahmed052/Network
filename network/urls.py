c3fbc9b20576600d9a0b87faa62c22f61a84cdcb
Dania187e <dania.abdelhalim@uoitc.edu.iq>
2023-12-22 02:10:16 +0300

    all files

--- network/urls.py
+++ network/urls.py
@@ -10,8 +10,11 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("newPost", views.newPost, name="newPost"),
     path("profile/<int:user_id>", views.profile, name="profile"),
-    path("unfollow", views.unfollow, name="unfollow"),
-    path("follow", views.follow, name="follow"),
+    path("unfollow/<int:user_id>", views.unfollow, name="unfollow"),
+    path("follow/<int:user_id>", views.follow, name="follow"),
     path("following", views.following, name="following"),
+    path("edit/<int:post_id>", views.edit, name="edit"),
+    path("remove_like/<int:post_id>", views.remove_like, name="remove_like"),
+    path("add_like/<int:post_id>", views.add_like, name="add_like"),
 
 ]
--- network/views.py
+++ network/views.py
@@ -4,24 +4,101 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.core.paginator import Paginator
+import json 
+from django.http import JsonResponse
 
-from .models import User, Post, Follow
+from .models import User, Post, Follow, Like
 
 
 def index(request):
     allPosts=Post.objects.all().order_by("id").reverse()
 
-
-    paginator=Paginator(allPosts, 1)
+    paginator=Paginator(allPosts, 10)
     page_number=request.GET.get('page')
     posts_of_the_page=paginator.get_page(page_number)
 
+    allLikes=Like.objects.all()
+    whoYouLiked=[]
+
+    try:
+       for like in allLikes:
+           if like.user.id==request.user.id:
+              whoYouLiked.append(like.post.id)
+    except:
+        whoYouLiked=[]
+
     return render(request, "network/index.html",{
-        "allPosts": allPosts,
         "posts_of_the_page": posts_of_the_page,
+        "whoYouLiked": whoYouLiked,
 
     })
 
+    
+    
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+
+def edit(request, post_id):
+    if request.method=="POST":
+        data= json.loads(request.body)
+        edit_post=Post.objects.get(pk=post_id)
+        edit_post.content=data["content"]
+        edit_post.save()
+        return JsonResponse({"message": " the change succesful" , "data": data["content"]})
+    else:
+        return JsonResponse({"message": " invalid request method " })
+    
+    
 def newPost(request):
     if request.method =="POST":
         content=request.POST['content']
@@ -30,6 +107,8 @@ def newPost(request):
         post.save()
         return HttpResponseRedirect(reverse(index))
     
+
+
 def profile(request, user_id):
     user=User.objects.get(pk=user_id)
     allPosts=Post.objects.filter(user=user).order_by("id").reverse()
@@ -88,7 +167,7 @@ def follow(request):
     f=Follow(user=currentUser, user_follower=userfollowData)
     f.save()
     user_id=userfollowData.id
-    return HttpResponseRedirect(reverse(profile, kwargs={'user_id': user_id}))
+    return HttpResponseRedirect(reverse(profile, kwargs={'user_id':user_id}))
 
 def unfollow(request):
     userfollow=request.POST['userfollow']
@@ -99,53 +178,16 @@ def unfollow(request):
     user_id=userfollowData.id
     return HttpResponseRedirect(reverse(profile, kwargs={'user_id':user_id}))
 
-def login_view(request):
-    if request.method == "POST":
+def remove_like(request, post_id):
+    post=Post.objects.get(pk=post_id)
+    user=User.objects.get(pk=request.user.id)
+    like=Like.objects.filter(user=user, post=post)
+    like.delete()
+    return JsonResponse({"message": "like removed "})
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "network/login.html")
-
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
-
-
-def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "network/register.html")
+def add_like(request, post_id):
+    post=Post.objects.get(pk=post_id)
+    user=User.objects.get(pk=request.user.id)
+    newLike=Like(user=user, post=post)
+    newLike.save()
+    return JsonResponse({"message": "like added "})
